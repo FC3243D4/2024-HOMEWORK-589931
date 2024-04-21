02217a8b08e34bbd8c17ee4ddf162282751748cb
FC3243D4 <colacicchi4@gmail.com>
2024-04-21 20:17:09 +0200

    aggiunta controllo del nome

--- test/it/uniroma3/diadia/ambienti/StanzaMagicaTest.java
+++ test/it/uniroma3/diadia/ambienti/StanzaMagicaTest.java
@@ -10,29 +10,40 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 public class StanzaMagicaTest {
 	private StanzaMagica stanzaMagicaSoglia0;
 	private StanzaMagica stanzaMagicaSogliaStandard;
-	private Attrezzo peso1;
+	private String nomeAttrezzo;
+	private String nomeAttrezzoInvertito;
+	private Attrezzo attrezzo;
 	private int pesoOriginale;
 	private int pesoDoppio;
 
 	@Before
 	public void setUp() {
+		this.nomeAttrezzo = new String("peso");
+		this.nomeAttrezzoInvertito = new StringBuilder(this.nomeAttrezzo).reverse().toString();
 		this.stanzaMagicaSoglia0 = new StanzaMagica("stanzaMagicaSoglia1",0);
 		this.stanzaMagicaSogliaStandard = new StanzaMagica("stanzaMagicaSogliaStandard");
-		this.peso1 = new Attrezzo("peso1",1);
-		this.pesoOriginale = this.peso1.getPeso();
+		this.attrezzo = new Attrezzo(this.nomeAttrezzo,1);
+		this.pesoOriginale = this.attrezzo.getPeso();
 		this.pesoDoppio = pesoOriginale*2;
 	}
 	
 	@Test
-	public void testAddAttrezzo_AttrezzoPosatoSogliaNonSuperata() {
-		assertTrue(this.stanzaMagicaSogliaStandard.addAttrezzo(peso1));
+	public void testAddAttrezzo_AttrezzoPosatoSogliaNonSuperataPesoENomeInalterati() {
+		assertTrue(this.stanzaMagicaSogliaStandard.addAttrezzo(attrezzo));
 		assertEquals(this.pesoOriginale,this.stanzaMagicaSogliaStandard.getAttrezzi()[0].getPeso());
+		assertEquals(this.nomeAttrezzo,this.stanzaMagicaSogliaStandard.getAttrezzi()[0].getNome());
 	}
 
 	@Test
 	public void testAddAttrezzoEModificaAttrezzo_AttrezzoPosatoConSogliaSuperataPesoDoppio() {
-		assertTrue(this.stanzaMagicaSoglia0.addAttrezzo(peso1));
+		assertTrue(this.stanzaMagicaSoglia0.addAttrezzo(attrezzo));
 		assertEquals(this.pesoDoppio,this.stanzaMagicaSoglia0.getAttrezzi()[0].getPeso());
+	}
+	
+	@Test
+	public void testAddAttrezzo_AttrezzoPosatoSogliaSuperataNomeInvertito() {
+		assertTrue(this.stanzaMagicaSoglia0.addAttrezzo(attrezzo));
+		assertEquals(this.nomeAttrezzoInvertito,this.stanzaMagicaSoglia0.getAttrezzi()[0].getNome());
 	}
 	
 	@Test
